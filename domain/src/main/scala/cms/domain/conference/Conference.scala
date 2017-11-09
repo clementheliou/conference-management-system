0207fdd77a6a34553978f5b06a99f128716c6be5
Clément HELIOU <cheliou@xebia.fr>
2017-11-09 22:25:41 +0100

    feat(conference): add basic conference update
    One can update the conference name.

--- domain/src/main/scala/cms/domain/conference/Conference.scala
+++ domain/src/main/scala/cms/domain/conference/Conference.scala
@@ -4,6 +4,9 @@ import cms.domain.eventsourcing.{Event, EventSourcedAggregate}
 
 final class Conference(val id: String, history: List[ConferenceEvent] = Nil) extends EventSourcedAggregate {
 
+  private[this] val state = new DecisionProjection
+  history foreach state.apply
+
   def this(name: String, slug: String) {
     this(slug)
     raise {
@@ -11,8 +14,25 @@ final class Conference(val id: String, history: List[ConferenceEvent] = Nil) ext
     }
   }
 
+  def update(name: String): Unit = raise {
+    ConferenceUpdated(name)
+  }
+
+  private class DecisionProjection {
+    var name, slug: String = _
+
+    def apply(event: ConferenceEvent): Unit = event match {
+      case e: ConferenceCreated =>
+        name = e.name
+        slug = e.slug
+
+      case e: ConferenceUpdated => name = e.name
+    }
+  }
 }
 
 sealed trait ConferenceEvent extends Event
 
 case class ConferenceCreated(name: String, slug: String) extends ConferenceEvent
+
+case class ConferenceUpdated(name: String) extends ConferenceEvent
--- domain/src/test/scala/cms/domain/conference/ConferenceTest.scala
+++ domain/src/test/scala/cms/domain/conference/ConferenceTest.scala
@@ -21,4 +21,17 @@ class ConferenceTest extends FlatSpec with Matchers {
     // Then
     conference.id should equal("mix-it-18")
   }
+
+  it can "be updated with a new name" in {
+
+    // Given
+    val history = List(ConferenceCreated(name = "MixIT 2018", slug = "mix-it-18"))
+    val conference = new Conference(id = "mix-it-18", history)
+
+    // When
+    conference.update(name = "MixIT 18'")
+
+    // Then
+    conference.events.map(_.event) should contain only ConferenceUpdated(name = "MixIT 18'")
+  }
 }
